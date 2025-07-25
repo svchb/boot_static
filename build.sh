ae1319068eddab718eb3d3fcf9f698761f005cd9
Sven Berger <berger.sven@gmail.com>
2025-07-25 18:35:11 +0200

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Builds for production into the 'docs/' folder, with BASEPATH pointing to GitHub Pages repo name
 
-REPO_NAME="YOUR_REPO_NAME"
+REPO_NAME="boot_static"
 chmod +x src/main.py
 python3 src/main.py "/${REPO_NAME}/"
